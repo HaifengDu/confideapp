0b42dda02091ea4718636de8b78174cb1a11847a
panduanlove <panduanlove@163.com>
2018-06-30 21:40:14 +0800

    功能：增加公共样式，增阿基其他资料设置

---
+++ app_static/src/page/settings/OtherInfo.vue
@@ -0,0 +1,142 @@
+<template>
+    <div class="container">
+        <div class="header">
+          其他资料
+        </div>
+        <div class="body">
+            <div class="list">
+                <mt-cell title="职业信息">
+                <select v-model="informations.job">
+                    <option :value="item.code" :key="index" v-for="(item,index) in jobs">{{item.name}}</option>
+                </select>
+                <i class="mint-cell-allow-right"></i>
+                </mt-cell>
+            </div>
+            <div class="list">
+                <mt-cell title="家庭状况">
+                <select v-model="informations.family">
+                    <option :value="item.code" :key="index" v-for="(item,index) in familyDatas">{{item.name}}</option>
+                </select>
+                <i class="mint-cell-allow-right"></i>
+                </mt-cell>
+            </div>
+            <div class="list">
+                <mt-cell title="教育水平">
+                <select v-model="informations.educate">
+                    <option :value="item.code" :key="index" v-for="(item,index) in educateDatas">{{item.name}}</option>
+                </select>
+                <i class="mint-cell-allow-right"></i>
+                </mt-cell>
+            </div>
+        </div>
+        <div class="back" @click="back"></div>
+        <mt-button type="primary" size="large" class="submit-btn" @click.native="submitInfo">提交资料</mt-button>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import BaseInfoService from "../../api/BaseInfoService";
+import { EBaseDataType } from '../../enum/EBaseDataType';
+
+@Component({
+ 
+})
+export default class OtherInfo extends Vue{
+    private service = BaseInfoService.getInstance();
+    private jobs:Array<any> = [];
+    private familyDatas:Array<any> = [];
+    private educateDatas:Array<any> = [];
+    private informations:any = {};
+    created(){
+        this.service.getArea(EBaseDataType.Job).then(res=>{
+            let data = res.data.data;
+            for(var key in data){
+                this.jobs.push({
+                    code:key,
+                    name:data[key].name
+                });
+            }
+        });
+        this.service.getArea(EBaseDataType.Family).then(res=>{
+            let data = res.data.data;
+            for(var key in data){
+                this.familyDatas.push({
+                    code:key,
+                    name:data[key].name
+                });
+            }
+        });
+        this.service.getArea(EBaseDataType.Edu).then(res=>{
+            let data = res.data.data;
+            for(var key in data){
+                this.educateDatas.push({
+                    code:key,
+                    name:data[key].name
+                });
+            }
+        });
+    }
+
+    back(){
+        (<any>this).$router.go(-1);
+    }
+    submitInfo(){
+        console.log(this.informations);
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    @import '../../assets/style.less';
+    *{
+        .f-nm;
+    }
+    .container{
+        .p-rl;
+    }
+    .header{
+        .v-middle(50px);
+        .f-lg;
+    }
+    .body{
+        .list{
+            text-align:left;
+            padding-left:1rem;
+            select{
+                border:none;
+                height:48px;
+                appearance:none;
+                -moz-appearance:none;
+                -webkit-appearance:none;
+                background-image: none;
+                background: no-repeat scroll right center transparent;
+                margin-right:25px;
+                color:#888;
+                font-size:1.4rem;
+                direction: rtl;
+            }
+            .mint-cell-value.is-link {
+                img{
+                height:4.2rem;
+                width:4.2rem;
+                border-radius:1.6rem;
+                }
+            }
+        }
+    }
+    .back{
+        width:20px;
+        height:20px;
+        .p-ab;
+        top:15px;
+        left:20px;
+        background:url(../../../static/images/userInfo/arrow-left-black.png) no-repeat center center;
+    }
+    .submit-btn{
+        border-radius:0;
+        position:fixed;
+        bottom:0;
+    }
+</style>

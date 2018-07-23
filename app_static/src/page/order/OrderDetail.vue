b2903904cc369dd6118f455d0330c94e4cf218c5
PanFengshan <pfs@acewill.cn>
2018-07-23 09:23:22 +0800

    功能：调整订单详情页面

--- app_static/src/page/order/OrderDetail.vue
+++ app_static/src/page/order/OrderDetail.vue
@@ -1,31 +1,40 @@
 <template>
-    <div class="container">
-        <div class="body">
-            <div class="type">{{serviceName}}</div>
-            <div class="main">
-                <div class="icon-container">
-                    <div class="icon">
-                        <img :src="getServiceTypeIcon()">
+    <div>
+        <div class="container con-bg">
+            <div class="body">
+                <div class="type">{{serviceName}}</div>
+                <div class="main">
+                    <div class="icon-container">
+                        <div class="icon">
+                            <img :src="getServiceTypeIcon()">
+                        </div>
                     </div>
+                    <div class="detail">
+                        <div class="status">
+                            <p class="status-text">{{statuNamesDic[status]}}</p>
+                            <p class="text">订单状态</p>
+                        </div>
+                        <div class="total">
+                            <p class="money">￥{{total}}</p>
+                            <p class="text">费用</p>
+                        </div>
+                    </div>
+                    <mt-cell title="倾听者" class="cell-con cell-prev">重新的开始</mt-cell>
+                    <mt-cell title="购买时长" class="cell-con cell-prev">15分钟</mt-cell>
+                    <mt-cell title="服务时长" class="cell-con cell-prev">15分钟</mt-cell>
+                    <mt-cell title="付款时间" class="cell-con cell-prev">2018-06-26 21:15</mt-cell>
+                    <mt-cell title="网络通话时长" class="cell-con cell-prev">00:15:00</mt-cell>
+                    <mt-cell title="优质专线时长" class="cell-con cell-prev">00:00:00</mt-cell>
+                    <mt-cell title="订单号" class="cell-con cell-prev">1509006</mt-cell>
+                    <mt-cell style="border-bottom:none;" title="有效期至" class="cell-con cell-prev">2018年06月29日 21:19</mt-cell>
                 </div>
-                <div class="detail">
-                    <div class="status">
-                        <p class="status-text">{{status}}</p>
-                        <p class="text">订单状态</p>
-                    </div>
-                    <div class="total">
-                        <p class="money">￥{{total}}</p>
-                        <p class="text">费用</p>
-                    </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="body">
+                <div class="header">
+                    <p>等待付款</p>
                 </div>
-                <mt-cell title="倾听者" class="cell-con cell-prev">重新的开始</mt-cell>
-                <mt-cell title="购买时长" class="cell-con cell-prev">15分钟</mt-cell>
-                <mt-cell title="服务时长" class="cell-con cell-prev">15分钟</mt-cell>
-                <mt-cell title="付款时间" class="cell-con cell-prev">2018-06-26 21:15</mt-cell>
-                <mt-cell title="网络通话时长" class="cell-con cell-prev">00:15:00</mt-cell>
-                <mt-cell title="优质专线时长" class="cell-con cell-prev">00:00:00</mt-cell>
-                <mt-cell title="订单号" class="cell-con cell-prev">1509006</mt-cell>
-                <mt-cell style="border-bottom:none;" title="有效期至" class="cell-con cell-prev">2018年06月29日 21:19</mt-cell>
             </div>
         </div>
     </div>
@@ -38,14 +47,17 @@ import {EPriceType} from "@/enum/EPriceType";
 @Component
 export default class OrderDetail extends Vue{
     private serviceName = '通话服务';
-    private status = '已结束';
+    private status = 1;
     private total = 9.9;
     private serviceType = 2;
+    private statuNamesDic = ['待支付','已结束 '];
+    private isToBePaid = false;
 
     create(){
         //TODO:根据订单单号获取订单支付状态，更具已支付还是未支付，展示对应的订单详情页面
         const orderid = this.$route.query.orderid;
         console.log(orderid);
+        this.isToBePaid = true;
     }
 
     getServiceTypeIcon(){
@@ -59,12 +71,14 @@ export default class OrderDetail extends Vue{
     @import '../../assets/style.less';
     @orange:rgb(239,146,55);
     @bgColor:rgb(238,238,238);
+    .con-bg{
+        background:@bgColor;
+    }
     *{
         .f-nm;
     }
     .container{
         height:100%;
-        background:@bgColor;
         .p-rl;
     }
     .body{

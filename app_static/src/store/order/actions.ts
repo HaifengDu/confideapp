3d9cfe978022ffa133a94759127fa70c79dc848d
PanFengshan <pfs@acewill.cn>
2018-08-02 10:22:26 +0800

    功能：增加order的store设置及文字服务设置调整

--- app_static/src/store/order/actions.ts
+++ app_static/src/store/order/actions.ts
@@ -1,7 +1,15 @@
 import { IOrderState } from "../../interface/IOrderState";
 import IRootState from "../../interface/IRootState";
 import { Action } from "vuex";
+import mutation_type from "./mutation_type";
 
 export const test:Action<IOrderState,IRootState> = ({dispatch,commit},payload)=>{
     //
 }
+
+export const setOrder: Action<IOrderState, IRootState> = (
+    { dispatch, commit },
+    order
+  ) => {
+    commit(mutation_type.UPDATE_ORDER, order);
+  };
